8054a192969b2d1ca2e7d8b019a5ce18651f53cd
Tim Verbelen <tim.verbelen@intec.ugent.be>
2016-03-07 16:24:59 +0100

    add message generation project

---
+++ be.iminds.iot.ros.msgs.generator/src/be/iminds/iot/ros/msgs/generator/MessageGenerator.java
@@ -0,0 +1,26 @@
+package be.iminds.iot.ros.msgs.generator;
+
+import org.osgi.service.component.annotations.Component;
+import org.ros.internal.message.GenerateInterfaces;
+
+/**
+ * Wrap a gogo command around the rosjava message generator
+ * 
+ * @author tverbele
+ *
+ */
+@Component(service = {Object.class},
+	property = {"osgi.command.scope=ros", 
+	"osgi.command.function=generate"},
+	immediate=true)
+public class MessageGenerator {
+	
+	public void generate(){
+		try {
+			GenerateInterfaces.main(new String[]{"generated_msgs"});
+		} catch(Throwable t){
+			t.printStackTrace();
+		}
+	}
+	
+}
