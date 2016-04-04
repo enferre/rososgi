e3a54013cbddf89aaa238a56fedddddc4828d259
Tim Verbelen <tim.verbelen@intec.ugent.be>
2016-04-04 20:51:19 +0200

    allow to define remappings when launching native ros nodes

--- be.iminds.iot.ros.api/src/be/iminds/iot/ros/util/NativeRosNode.java
+++ be.iminds.iot.ros.api/src/be/iminds/iot/ros/util/NativeRosNode.java
@@ -1,5 +1,8 @@
 package be.iminds.iot.ros.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Deactivate;
 
@@ -9,16 +12,35 @@ public class NativeRosNode {
 	
 	protected final String rosPackage;
 	protected final String rosNode;
+	protected final List<String> rosMappings = new ArrayList<>();
 	
 	public NativeRosNode(String pkg, String node){
 		this.rosPackage = pkg;
 		this.rosNode = node;
 	}
 	
+	public NativeRosNode(String pkg, String node, String... mappings){
+		this.rosPackage = pkg;
+		this.rosNode = node;
+		
+		for(String mapping : mappings){
+			if(!mapping.contains(":=")){
+				System.out.println("Invalid mapping: "+mapping);
+				continue;
+			} 
+			rosMappings.add(mapping);
+		}
+	}
+	
 	@Activate
 	protected void activate() throws Exception {
 		try {
-			ProcessBuilder builder = new ProcessBuilder("rosrun", rosPackage, rosNode);
+			List<String> cmd = new ArrayList<>();
+			cmd.add("rosrun");
+			cmd.add(rosPackage);
+			cmd.add(rosNode);
+			cmd.addAll(rosMappings);
+			ProcessBuilder builder = new ProcessBuilder(cmd);
 			builder.inheritIO();
 			process = builder.start();
 		} catch(Exception e){
