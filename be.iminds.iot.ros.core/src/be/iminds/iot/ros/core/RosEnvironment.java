a83099367432686832bdf86602d6623adf7d506a
Tim Verbelen <tim.verbelen@intec.ugent.be>
2016-04-05 11:44:54 +0200

    initialize roscore in RosEnvironment and block until initialized

--- be.iminds.iot.ros.core/src/be/iminds/iot/ros/core/RosEnvironment.java
+++ be.iminds.iot.ros.core/src/be/iminds/iot/ros/core/RosEnvironment.java
@@ -1,6 +1,7 @@
 package be.iminds.iot.ros.core;
 
 import java.io.File;
+import java.net.Socket;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +9,8 @@ import java.util.List;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.ros.RosCore;
 
 import be.iminds.iot.ros.api.Environment;
 
@@ -15,11 +18,16 @@ import be.iminds.iot.ros.api.Environment;
 		"osgi.command.function=env"})
 public class RosEnvironment implements Environment {
 
+	// ROS environment variables
 	private URI masterURI;
 	private String distro;
 	private String namespace;
 	private String root;
 	private String packagePath;
+	
+	// ROS core instance (native or rosjava)
+	private Process nativeCore;
+	private RosCore core;
 	
 	@Activate
 	void activate(BundleContext context) throws Exception {
@@ -37,6 +45,56 @@ public class RosEnvironment implements Environment {
 			System.err.println("Error setting up the ROS environment: "+e.getMessage());
 			throw e;
 		} 
+		
+		// start ROS core if required
+		boolean start = !rosCoreActive();
+		
+		if(start){
+			boolean startNative = false;
+			String rosCoreNative = context.getProperty("ros.core.native");
+			if(rosCoreNative != null){
+				startNative = Boolean.parseBoolean(rosCoreNative);
+			}
+			
+			if(startNative){
+				// native ROScore process
+				ProcessBuilder builder = new ProcessBuilder("roscore");
+				builder.inheritIO();
+				nativeCore = builder.start();
+			} else {
+				// rosjava ROScore implementation
+				core = RosCore.newPublic(masterURI.getHost(), masterURI.getPort());
+				core.start();
+				System.out.println("ROS core service [/rosout] started on "+core.getUri());
+			}
+		}
+		
+		// block until ROS core is initialized
+		while(!rosCoreActive()){
+			Thread.sleep(500);
+		}
+	}
+	
+	@Deactivate
+	void deactivate() throws Exception {
+		if(core != null)
+			core.shutdown();
+		
+		if(nativeCore != null){
+			nativeCore.destroy();
+		}
+	}
+	
+	private boolean rosCoreActive(){
+		try {
+			// try to connect to ROS core
+			Socket s = new Socket(masterURI.getHost(), masterURI.getPort());
+			s.close();
+			return true;
+		} catch(Exception e){
+			// not active
+			return false;
+		}
 	}
 	
 	private String getVariable(String environmentKey, String propertyKey, BundleContext context){
