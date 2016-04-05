73f8a6d9554fa20cb0f7bb5349a4b771c922c4db
Tim Verbelen <tim.verbelen@intec.ugent.be>
2016-04-05 09:45:15 +0200

    also allow to start native roscore (rosjava impl has missing features)

--- be.iminds.iot.ros.core/src/be/iminds/iot/ros/core/RosCoreInit.java
+++ be.iminds.iot.ros.core/src/be/iminds/iot/ros/core/RosCoreInit.java
@@ -16,7 +16,11 @@ import be.iminds.iot.ros.api.Environment;
 public class RosCoreInit {
 
 	private final static String DEFAULT_URI = "http://localhost:11311";
+	
+	// choose between Java and native ROS core
+	private Process nativeCore;
 	private RosCore core;
+	
 	private Environment env;
 	
 	@Activate
@@ -25,20 +29,45 @@ public class RosCoreInit {
 		if(masterURI==null){
 			masterURI = new URI(DEFAULT_URI);
 		}
+		boolean start = true;
 		try {
 			// try to connect first to see whether there is already something running there
 			Socket s = new Socket(masterURI.getHost(), masterURI.getPort());
+			start = false;
 			s.close();
 		} catch(Exception e){
-			core = RosCore.newPublic(masterURI.getHost(), masterURI.getPort());
-			core.start();
-			System.out.println("ROS core service [/rosout] started on "+core.getUri());
+			System.out.println("ROS core already running...");
+		}
+		
+		if(start){
+			boolean startNative = false;
+			String rosCoreNative = context.getProperty("ros.core.native");
+			if(rosCoreNative != null){
+				startNative = Boolean.parseBoolean(rosCoreNative);
+			}
+			
+			if(startNative){
+				// native ROScore process
+				ProcessBuilder builder = new ProcessBuilder("roscore");
+				builder.inheritIO();
+				nativeCore = builder.start();
+			} else {
+				// rosjava ROScore implementation
+				core = RosCore.newPublic(masterURI.getHost(), masterURI.getPort());
+				core.start();
+				System.out.println("ROS core service [/rosout] started on "+core.getUri());
+			}
 		}
 	}
 
 	@Deactivate
 	void deactivate() throws Exception {
-		core.shutdown();
+		if(core != null)
+			core.shutdown();
+		
+		if(nativeCore != null){
+			nativeCore.destroy();
+		}
 	}
 
 	@Reference
