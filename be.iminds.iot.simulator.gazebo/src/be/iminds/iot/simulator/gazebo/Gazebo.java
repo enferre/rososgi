5c547bb6e3edb04f3c180a1b61723c3ffc6ee152
Tim Verbelen <tim.verbelen@intec.ugent.be>
2017-05-29 11:25:51 +0200

    allow to "change" the scene with consecutive loadScene calls

--- be.iminds.iot.simulator.gazebo/src/be/iminds/iot/simulator/gazebo/Gazebo.java
+++ be.iminds.iot.simulator.gazebo/src/be/iminds/iot/simulator/gazebo/Gazebo.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 import org.osgi.util.promise.Deferred;
+import org.osgi.util.promise.Promise;
 import org.ros.exception.RemoteException;
 import org.ros.message.MessageListener;
 import org.ros.message.Time;
@@ -17,6 +18,8 @@ import org.ros.node.topic.Subscriber;
 import be.iminds.iot.simulator.api.Orientation;
 import be.iminds.iot.simulator.api.Position;
 import be.iminds.iot.simulator.api.Simulator;
+import gazebo_msgs.DeleteModelRequest;
+import gazebo_msgs.DeleteModelResponse;
 import gazebo_msgs.SpawnModelRequest;
 import gazebo_msgs.SpawnModelResponse;
 import geometry_msgs.Point;
@@ -39,6 +42,7 @@ public class Gazebo implements Simulator {
 	private ServiceClient<gazebo_msgs.SpawnModelRequest, gazebo_msgs.SpawnModelResponse> spawnSDFModel;
 	private ServiceClient<gazebo_msgs.SpawnModelRequest, gazebo_msgs.SpawnModelResponse> spawnURDFModel;
 	private ServiceClient<gazebo_msgs.SpawnModelRequest, gazebo_msgs.SpawnModelResponse> spawnGazeboModel;
+	private ServiceClient<gazebo_msgs.DeleteModelRequest, gazebo_msgs.DeleteModelResponse> deleteModel;
 
 	
 	private Subscriber<rosgraph_msgs.Clock> clock; 
@@ -49,6 +53,8 @@ public class Gazebo implements Simulator {
 	private long millis = 0;
 	private long step = 100; // TODO make this configurable?
 	
+	private volatile String scene = null;
+	
 	public Gazebo(ConnectedNode node) throws Exception{
 		start = node.newServiceClient("/gazebo/unpause_physics", std_srvs.Empty._TYPE);
 		stop = node.newServiceClient("/gazebo/pause_physics",  std_srvs.Empty._TYPE);
@@ -56,6 +62,7 @@ public class Gazebo implements Simulator {
 		spawnSDFModel = node.newServiceClient("/gazebo/spawn_sdf_model", gazebo_msgs.SpawnModel._TYPE);
 		spawnURDFModel = node.newServiceClient("/gazebo/spawn_urdf_model", gazebo_msgs.SpawnModel._TYPE);
 		spawnGazeboModel = node.newServiceClient("/gazebo/spawn_gazebo_model", gazebo_msgs.SpawnModel._TYPE);
+		deleteModel = node.newServiceClient("/gazebo/delete_model", gazebo_msgs.DeleteModel._TYPE);
 
 		
 		clock = node.newSubscriber("/clock", rosgraph_msgs.Clock._TYPE);
@@ -160,8 +167,44 @@ public class Gazebo implements Simulator {
 
 	@Override
 	public void loadScene(String file, Map<String, String> config) {
-		// TODO introduce separate spawn_model function in Simulator?!
-		// TODO could this load world files similar to what roslaunch does?!
+		// in case of gazebo we see the "scene" as a single model that can be loaded
+		// if we load a new scene, we first delete the previous "scene" 
+		
+		if(scene != null){
+			try {
+				deleteModel(scene).getValue();
+			} catch (Exception e) {
+			}
+		}
+		
+		Position position = null;
+		Orientation orientation = null;
+	
+		if(config.containsKey("pose")){
+			String pose = config.get("pose");
+			String[] numbers = pose.split(",");
+			if(numbers.length >= 3){
+				position = new Position();
+				position.x = Float.parseFloat(numbers[0].trim());
+				position.y = Float.parseFloat(numbers[1].trim());
+				position.z = Float.parseFloat(numbers[2].trim());
+			} 
+			if(numbers.length == 6){
+				orientation = new Orientation();
+				orientation.alfa = Float.parseFloat(numbers[3].trim());
+				orientation.beta = Float.parseFloat(numbers[4].trim());
+				orientation.gamma = Float.parseFloat(numbers[5].trim());
+			}
+		}			
+			
+		try {
+			spawnModel(file, position, orientation).getValue();
+		} catch (Exception e) {
+		}
+	
+	}
+	
+	private Promise<Void> spawnModel(String file, Position position, Orientation orientation){
 		ServiceClient<SpawnModelRequest, SpawnModelResponse> spawnModel = null;
 		if(file.endsWith(".sdf")){
 			spawnModel = spawnSDFModel;
@@ -170,37 +213,51 @@ public class Gazebo implements Simulator {
 		} else {
 			spawnModel = spawnGazeboModel;
 		}
-		
-		try {
-			final Deferred<Void> deferred = new Deferred<>();
+
+		final Deferred<Void> deferred = new Deferred<>();
 			
+		try {
 			File f = new File(file);
 			byte[] bytes = Files.readAllBytes(f.toPath());
 			String xml = new String(bytes);
 			
 			SpawnModelRequest req = spawnModel.newMessage();
-			req.setModelName(f.getName());
+			final String name = f.getName();
+			req.setModelName(name);
 			req.setModelXml(xml);
 			Pose p = req.getInitialPose();
-			Point point = p.getPosition();
-			Quaternion quaternion = p.getOrientation();
-			if(config.containsKey("pose")){
-				String pose = config.get("pose");
-				String[] numbers = pose.split(",");
-				if(numbers.length >= 3){
-					point.setX(Double.parseDouble(numbers[0].trim()));
-					point.setY(Double.parseDouble(numbers[1].trim()));
-					point.setZ(Double.parseDouble(numbers[2].trim()));
-				} 
-				if(numbers.length == 7){
-					quaternion.setW(Double.parseDouble(numbers[3].trim()));
-					quaternion.setX(Double.parseDouble(numbers[4].trim()));
-					quaternion.setY(Double.parseDouble(numbers[5].trim()));
-					quaternion.setZ(Double.parseDouble(numbers[6].trim()));
-				}
+
+			if(position != null){
+				Point point = p.getPosition();
+				point.setX(position.x);
+				point.setY(position.y);
+				point.setZ(position.z);
 				p.setPosition(point);
-				p.setOrientation(quaternion);
-			}			
+			}
+			
+			if(orientation != null){
+				Quaternion q = p.getOrientation();
+				// Converting euler angles to quaternion
+				// Assuming euler angles in radians!
+				double c1 = Math.cos(orientation.alfa/2);
+				double s1 = Math.sin(orientation.alfa/2);
+				double c2 = Math.cos(orientation.beta/2);
+				double s2 = Math.sin(orientation.beta/2);
+				double c3 = Math.cos(orientation.gamma/2);
+				double s3 = Math.sin(orientation.gamma/2);
+				double c1c2 = c1*c2;
+				double s1s2 = s1*s2;
+				double w =c1c2*c3 - s1s2*s3;
+				double x =c1c2*s3 + s1s2*c3;
+				double y =s1*c2*c3 + c1*s2*s3;
+				double z =c1*s2*c3 - s1*c2*s3;
+				q.setW(w);
+				q.setX(x);
+				q.setY(y);
+				q.setZ(z);
+				p.setOrientation(q);
+			}
+			
 			req.setInitialPose(p);
 
 			spawnModel.call(req, new ServiceResponseListener<SpawnModelResponse>() {
@@ -212,17 +269,43 @@ public class Gazebo implements Simulator {
 				@Override
 				public void onSuccess(SpawnModelResponse resp) {
 					if(resp.getSuccess()){
+						Gazebo.this.scene = name;
 						deferred.resolve(null);
 					} else {
 						deferred.fail(new Exception(resp.getStatusMessage()));
 					}
 				}
 			});
-			
-			deferred.getPromise().getValue();
 		} catch(Exception e){
-			e.printStackTrace();
+			deferred.fail(e);
 		}
+		
+		return deferred.getPromise();
+	}
+	
+	private Promise<Void> deleteModel(String modelName){
+		final Deferred<Void> deferred = new Deferred<>();
+		
+		DeleteModelRequest req = deleteModel.newMessage();
+		req.setModelName(modelName);
+		
+		deleteModel.call(req, new ServiceResponseListener<DeleteModelResponse>() {
+			@Override
+			public void onFailure(RemoteException ex) {
+				deferred.fail(ex);
+			}
+
+			@Override
+			public void onSuccess(DeleteModelResponse resp) {
+				if(resp.getSuccess()){
+					deferred.resolve(null);
+				} else {
+					deferred.fail(new Exception(resp.getStatusMessage()));
+				}
+			}
+		});
+		
+		return deferred.getPromise();
 	}
 
 	@Override
