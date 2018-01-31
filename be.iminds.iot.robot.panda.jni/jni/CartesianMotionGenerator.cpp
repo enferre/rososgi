e95db4950a3f1bcb621889e45f42d3721e6ae359
Tim Verbelen <tim.verbelen@intec.ugent.be>
2018-01-31 13:08:24 +0100

    cartesian pose fixed with 0.2.0 update from libfranka

--- be.iminds.iot.robot.panda.jni/jni/CartesianMotionGenerator.cpp
+++ be.iminds.iot.robot.panda.jni/jni/CartesianMotionGenerator.cpp
@@ -27,10 +27,8 @@ franka::CartesianPose CartesianMotionGenerator::next(
 		double adistance = orientation_start.angularDistance(orientation_goal);
 
 		// TODO which are the best v/a max values?
-		// TODO also incorporate orientation change here?
 		double v_max = 2;
 		double a_max = 2.5;
-
 		double vo_max = 2.6;
 		double ao_max = 5;
 
@@ -55,27 +53,7 @@ franka::CartesianPose CartesianMotionGenerator::next(
 	Eigen::Quaterniond orientation = orientation_start.slerp(s, orientation_goal);
 	orientation.normalize();
 
-	// nlerp implementation?
-//	Eigen::Quaterniond orientation;
-//	double dot = orientation_start.dot(orientation_goal);
-//	double si = 1-s;
-//	if(dot < 0){
-//		orientation.x() = si*orientation_start.x() - s*orientation_goal.x();
-//		orientation.y() = si*orientation_start.y() - s*orientation_goal.y();
-//		orientation.z() = si*orientation_start.z() - s*orientation_goal.z();
-//		orientation.w() = si*orientation_start.w() - s*orientation_goal.w();
-//
-//	} else {
-//		orientation.x() = si*orientation_start.x() + s*orientation_goal.x();
-//		orientation.y() = si*orientation_start.y() + s*orientation_goal.y();
-//		orientation.z() = si*orientation_start.z() + s*orientation_goal.z();
-//		orientation.w() = si*orientation_start.w() + s*orientation_goal.w();
-//	}
-//	orientation.normalize();
-
-	// TODO orientation interpolation not working properly?
 	Eigen::Matrix3d mat = orientation.toRotationMatrix();
-	//Eigen::Matrix3d mat = orientation_start.toRotationMatrix();
 
 	std::array<double, 16> new_pose;
 	// TODO is there a better way here?
@@ -99,12 +77,10 @@ franka::CartesianPose CartesianMotionGenerator::next(
 	new_pose[14] = position(2);
 	new_pose[15] = 1;
 
-
 //	std::cout << new_pose[0] << " " << new_pose[1] << " " << new_pose[2] << " " << new_pose[3] << std::endl;
 //	std::cout << new_pose[4] << " " << new_pose[5] << " " << new_pose[6] << " " << new_pose[7]<< std::endl;
 //	std::cout << new_pose[8] << " " << new_pose[9] << " " << new_pose[10] << " " << new_pose[11] << std::endl;
 //	std::cout << new_pose[12] << " " << new_pose[13] << " " << new_pose[14] << " " << new_pose[15]<< std::endl << std::endl;
-
 
 	franka::CartesianPose output(new_pose);
 	if (time_ >= T) {
